80cca4f68e6e7d85d3212e8b3d1060a6961e5431
Akesh Mistry <akesh.mistry15@gmail.com>
2023-11-29 22:13:35 +0000

    Run Config37

---
+++ job_data/8553744.pbs/sim/run.py
@@ -0,0 +1,69 @@
+from simulation import Simulation, UniversalCellParams
+from conf import path_to_data
+from inputs import get_sim_configuration
+
+
+def create_simulation(config_name=None):
+    cf = get_sim_configuration(config_name)
+
+    cf.no_possible_phenotypes = int(cf.no_possible_phenotypes)
+
+    tumour_universal_params = UniversalCellParams(
+        cf.tumour_natural_prolif_rate,
+        cf.tumour_natural_death_rate,
+        cf.tumour_interaction_induced_rate,
+        cf.tumour_selectivity,
+    )
+
+    CTL_universal_params = UniversalCellParams(
+        cf.CTL_natural_prolif_rate,
+        cf.CTL_natural_death_rate,
+        cf.CTL_interaction_induced_rate,
+        cf.CTL_selectivity,
+    )
+
+    sim = Simulation(
+        cf.time_step,
+        cf.final_time,
+        int(cf.no_possible_phenotypes),
+        1,
+        no_init_tumour_cells=int(cf.no_init_tumour_cells),
+        no_init_CTL_cells=int(cf.no_init_CTL_cells),
+        tumour_universal_params=tumour_universal_params,
+        CTL_universal_params=CTL_universal_params,
+        TCR_affinity_range=cf.affinity_range,
+        TCR_binding_affinity=cf.binding_affinity,
+        tumour_phenotypic_variation_probability=cf.tumour_phenotypic_variation_probability,
+        config_name=cf.name,
+    )
+    return sim
+
+
+def run(overwrite=None, config_name=None):
+    create_new = False
+    try:
+        sim = Simulation.load_simulation(path_to_data)
+        if overwrite is None:
+            overwrite = input("Would you like to overwrite this simulation?")
+
+        if overwrite == "y":
+            create_new = True
+    except IOError:
+        create_new = True
+
+    if create_new:
+        print("Creating a new simulation.")
+        sim = create_simulation(config_name)
+    sim.run()
+
+    Simulation.save_simulation(path_to_data, sim)
+
+
+def extend(additional_time):
+    try:
+        sim = Simulation.load_simulation(path_to_data)
+        sim.extend(additional_time)
+        Simulation.save_simulation(path_to_data, sim)
+        print("Simulation extended.")
+    except IOError:
+        print("Simuation to extend could not be found")
