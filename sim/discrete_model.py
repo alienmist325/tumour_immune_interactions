604204810736fd3800fce4ea1bfa45b86edf036c
Akesh <akeshmistry15@gmail.com>
2024-02-28 00:45:16 +0000

    Add new configuration options, implement Interactions fully, resolve equality bugs when running simulation

--- sim/discrete_model.py
+++ sim/discrete_model.py
@@ -1,327 +1,18 @@
-import random
+"""
+My new proposed discrete model, with a flexible phenotype space.
+"""
+
+
 import numpy as np
 from copy import deepcopy
-import conf
+import sim.config.conf as conf
 import importlib
 import pickle
-from abc import ABC, abstractmethod
-import Levenshtein
+from phenotype import Phenotype, PhenotypeInteractions, PhenotypeStructure, SequencePhenotypeStructure, LatticePhenotypeStructure
 
 # Rename this to discrete_model eventually
 
 
-class PhenotypeStructure(ABC):
-    @abstractmethod
-    def get_random_phenotype(self):
-        """
-        Generate a valid phenotype id.
-        """
-        pass
-
-    @abstractmethod
-    def get_random_mutation(self, phenotype):
-        """
-        Get a possible mutation of the phenotype.
-        """
-        pass
-
-    @abstractmethod
-    def get_value(self, phenotype):
-        """
-        Get the value associated with a phenotype id.
-        """
-        return phenotype.id  # By default, these are the same
-
-    # (Not anymore) IDEA: We want to have a general function here which will take in the two ids and get the scaling. If they're the same type, we should redirect back down to the
-    # relevant class, since e.g. LatticePhenotypeStructure _can definitely_ compare between two lattice phenotypes. If they're different, we'll cover it here and overload.
-
-
-class Phenotype:
-    def __init__(self, struct: PhenotypeStructure, id):
-        # Notice we will have a problem if our value isn't one that compares nicely (e.g. float), so we have to use a "comparable" id here instead
-        self.struct = struct
-        self.id = id
-
-    def __eq__(self, other):
-        return (self.struct == other.struct) and (self.id == other.id)
-
-    def get_value(self):
-        return self.struct.get_value(self)
-
-    def get_random_mutation(self):
-        return self.struct.get_random_mutation(self)
-
-    def __hash__(self):
-        return hash((self.struct, self.id))
-
-    def __str__(self):
-        return f"{self.id} inside {str(self.struct)}"
-
-
-class SequencePhenotypeStructure(PhenotypeStructure):
-    def __init__(self, sequences):
-        self.sequences = sequences
-
-    def get_random_phenotype(self):
-        """
-        Generate a valid phenotype id.
-        """
-        return random.choice(self.sequences)
-
-    def get_random_mutation(self, phenotype):
-        """
-        Get a possible mutation of the phenotype.
-        """
-        return random.choice(self.sequences, self.get_mutation_weightings())
-    
-    def get_mutation_weightings(self, phenotype):
-        return np.ones(len(self.sequences))
-
-    def get_value(self, phenotype):
-        """
-        Get the value associated with a phenotype id.
-        """
-        return phenotype.id  # By default, these are the same
-
-    @classmethod
-    def get_affinity_distance(self, phen_1, phen_2, binding_affinity_matrix):
-        # For comparing different seqeuence phenotypes (i.e. tumour and T-cell), using binding affinities
-        pass
-
-    @classmethod
-    def get_sequence_distance(self, phen_1: Phenotype, phen_2: Phenotype, sequence_matrix):
-        # TODO: implement a sequence matrix
-        return Levenshtein.distance(phen_1.get_value(), phen_2.get_value())
-
-    @classmethod
-    def get_interaction_function(self):
-        return (
-            self.get_sequence_distance
-        )  # or is it better to do self.etc rather than the class method? Which works?
-
-    @classmethod
-    def get_standard_interaction_data(self):
-        # An example of some data
-        # Is this the best way to do this? In truth, we just need a specification of _what_ we need to provide
-        sequence_matrix = np.identity(10)  # TODO: fix this
-        return sequence_matrix
-
-    @classmethod
-    def get_cross_interaction_function(self):
-        return self.get_affinity_distance
-
-    @classmethod
-    def get_standard_cross_interaction_data(self):
-        # An example of some data
-        # Is this the best way to do this? In truth, we just need a specification of _what_ we need to provide
-        affinity_matrix = np.identity(10)  # TODO: fix this
-        return affinity_matrix
-
-
-class LatticePhenotypeStructure(PhenotypeStructure):
-    """
-    Phenotypes are in the range [0,1], and are floats
-    Phenotype IDs are in the range [0, no_possible_values - 1], and are integers
-    """
-
-    def __init__(self, abs_max_value, no_possible_values):
-        self.abs_max_value = abs_max_value
-        self.range = np.linspace(
-            -self.abs_max_value, self.abs_max_value, no_possible_values, True
-        )
-        self.id_range = range(no_possible_values)
-        self.step_size = 2 * abs_max_value / no_possible_values  # check this
-        self.no_possible_values = no_possible_values  # For an id
-
-    """
-    def shift(self, phen, no_steps, direction):
-        print(phen)
-        phen += no_steps * direction * self.step_size
-        if phen > self.abs_max_value:
-            phen = self.abs_max_value
-        if phen < -self.abs_max_value:
-            phen = -self.abs_max_value
-        return phen
-    """
-
-    def shift(self, phen: Phenotype, no_steps, direction):
-        phen_id = phen.id
-        phen_id += no_steps * direction
-        if phen_id > self.no_possible_values - 1:
-            phen_id = self.no_possible_values - 1
-        elif phen_id < 0:
-            phen_id = 0
-        return Phenotype(phen.struct, phen_id)
-
-    def get_value(self, phen: Phenotype):
-        id = phen.id
-        return (id * self.step_size) - self.abs_max_value
-
-    def get_random_phenotype(self):
-        return Phenotype(self, random.randint(0, self.no_possible_values - 1))
-
-    def get_random_mutation(self, phenotype: Phenotype):
-        no_steps = 1
-        direction = random.choice([-1, 1])
-        return self.shift(phenotype, no_steps, direction)
-
-    @classmethod
-    def is_excluded_phenotype(self, phenotype: Phenotype, exclude_percent=0.1):
-        phen_struct = phenotype.struct
-        phenotype_id = phenotype.id
-        no_values = phen_struct.no_possible_values
-        return (
-            phenotype_id < no_values * exclude_percent
-            or phenotype_id > no_values * (1 - exclude_percent)
-        )
-
-    @classmethod
-    def get_circular_distance(self, a, b):
-        """
-        Get the circular distance on [0,1] between a and b.
-        """
-        if b < a:
-            # Switch so a < b
-            temp = a
-            a = b
-            b = temp
-        return min(abs(b - a), abs(a + 1 - b))
-
-    @classmethod
-    def get_interaction_function(self):
-        return (
-            self.compute_interaction_scaling
-        )  # or is it better to do self.etc rather than the class method? Which works?
-
-    @classmethod
-    def get_standard_interaction_data(self):
-        # An example of some data
-        # Is this the best way to do this? In truth, we just need a specification of _what_ we need to provide
-        distance_type = "length"
-        return distance_type
-
-    @classmethod
-    def compute_interaction_scaling(
-        self, phenotype_1_, phenotype_2_, range, distance_type="line"
-    ):
-        """
-        Return 0 if phenotypes are out of the selectivity range; otherwise return a constant, modified to account for the boundary.
-        """
-        phenotype_1 = phenotype_1_.get_value()
-        phenotype_2 = phenotype_2_.get_value()
-        if distance_type == "line":
-            distance = abs(phenotype_1 - phenotype_2)
-        if distance_type == "circular":
-            distance = LatticePhenotypeStructure.get_circular_distance(
-                phenotype_1, phenotype_2
-            )
-        if distance <= range:
-            return 1 / (
-                min(phenotype_1 + range, self.abs_max_value)
-                - max(phenotype_1 - range, -self.abs_max_value)
-            )
-        else:
-            return 0
-
-    """
-    def get_random_phenotype(self):
-        return (
-            random.randint(0, self.no_possible_values - 1) * self.step_size
-        ) - self.abs_max_value
-    """
-
-
-class PhenotypeInteractions:
-    """
-    Outlines how the different PhenotypeStructure objects in the program interact with each other.
-
-    interaction_data : dict from (struct1, struct2) to data
-    """
-
-    def __init__(self, interaction_data: dict = {}, interaction_functions: dict = {}):
-        self.interaction_data = interaction_data
-        self.interaction_functions = interaction_functions
-        self.phenotype_separation_scaling = {}
-
-    def compute_interaction_scaling(self, phen_1: Phenotype, phen_2: Phenotype, range):
-        struct_tuple = (phen_1.struct, phen_2.struct)
-        data = self.interaction_data[struct_tuple]
-        function = self.interaction_functions[struct_tuple]
-        function(phen_1, phen_2, range, data)
-
-    def setup_interactions(self, struct_tuple, data, function):
-        self.interaction_data[struct_tuple] = data
-        self.interaction_functions[struct_tuple] = function
-
-    def get_interaction_scaling(
-        self,
-        phenotype_1,
-        phenotype_2,
-        range,
-    ):
-        if (
-            phenotype_1,
-            phenotype_2,
-        ) not in self.phenotype_separation_scaling:
-            self.phenotype_separation_scaling[
-                (phenotype_1, phenotype_2)
-            ] = self.compute_interaction_scaling(phenotype_1, phenotype_2, range)
-        return self.phenotype_separation_scaling[(phenotype_1, phenotype_2)]
-
-    @classmethod
-    def get_default_sequence_interactions(
-        self,
-        tumour_struct: SequencePhenotypeStructure,
-        CTL_struct: SequencePhenotypeStructure,
-        sequence_matrix,
-        affinity_matrix,
-    ):
-        """
-        Create a PhenotypeInteractions object which handles interactions between two sequence-based phenotype structures.
-        """
-        ts = tumour_struct
-        cs = CTL_struct
-        phen_int = PhenotypeInteractions()
-
-        phen_int.setup_interactions(
-            (ts, ts),
-            sequence_matrix,
-            SequencePhenotypeStructure.get_interaction_function(),
-        )
-
-        phen_int.setup_interactions(
-            (cs, cs),
-            sequence_matrix,
-            SequencePhenotypeStructure.get_interaction_function(),
-        )
-
-        phen_int.setup_interactions(
-            (ts, cs),
-            affinity_matrix,
-            SequencePhenotypeStructure.get_cross_interaction_function(),
-        )
-
-        phen_int.setup_interactions(
-            (cs, ts),
-            np.transpose(affinity_matrix),
-            SequencePhenotypeStructure.get_cross_interaction_function(),
-        )
-
-        return phen_int()
-
-    @classmethod
-    def get_default_lattice_interactions(
-        self, lattice_struct: LatticePhenotypeStructure
-    ):
-        """
-        Create a PhenotypeInteractions object which handles interactions where all cells have the same lattice phenotype structure.
-        """
-        ls = lattice_struct
-        phen_int = PhenotypeInteractions()
-
-        phen_int.setup_interactions(
-            (ls, ls), ls.get_standard_interaction_data(), ls.get_interaction_function()
-        )
 
 
 class UniversalCellParams:
@@ -358,7 +49,6 @@ class CellBundle:
         if PhenotypeStructure.is_excluded_phenotype(self.phen_struct, phenotype):
             return
         """
-        print(phenotype)
 
         if phenotype not in self.cells_at_phenotype:
             self.cells_at_phenotype[phenotype] = number
@@ -366,15 +56,10 @@ class CellBundle:
             self.cells_at_phenotype[phenotype] += number
 
     def kill_cells(self, phenotype: Phenotype, number):
-        print("killing" + str(phenotype))
-        from debug import print_dict_with_function
-
-        print_dict_with_function(
-            self.cells_at_phenotype, lambda phenotype: str(phenotype)
-        )
+        
         if phenotype not in self.cells_at_phenotype:
             raise ValueError(
-                "No cells of this phenotype exist. Cannot kill cells."
+                "No cells of this phenotype exist. Cannot kill cells. "
                 + str(phenotype.id)
             )
         else:
@@ -411,8 +96,6 @@ class CellBundle:
     ):
         new_cells = deepcopy(cells)
         for phenotype, number in cells.cells_at_phenotype.items():
-            # print(number)
-            # number = 100 * number
             """
             if PhenotypeStructure.is_excluded_phenotype(
                 cells.phen_struct, phenotype
@@ -420,7 +103,6 @@ class CellBundle:
                 continue
             """
             weights = get_phenotype_probabilities(phenotype)
-            # print(weights)
             rng = np.random.default_rng()
             births, deaths, quiescences = rng.multinomial(number, weights)
             # print(births, "|", deaths, "|", quiescences)
@@ -456,7 +138,7 @@ class SimulationState:
     }
 
     def __init__(self, CTL_cells: CellBundle, tumour_cells: CellBundle):
-        from conf import sim_state_init_type
+        from sim.config.conf import sim_state_init_type
 
         initialiser = SimulationState.type_to_init_dict[sim_state_init_type]
         self = initialiser(self, CTL_cells, tumour_cells)
@@ -468,7 +150,7 @@ class SimulationHistory:
 
     def __init__(self, history: list[SimulationState] = []):
         # Do I need to copy the simulation?
-        from conf import sim_state_init_type
+        from sim.config.conf import sim_state_init_type
 
         self.history = history
         self.state_init_type = sim_state_init_type  # We hope this is the same as each sim state, but technically, it could not be
@@ -508,7 +190,7 @@ class Simulation:
         self.time_step = 0  # An integer describing which time step we're on
         self.final_time_step = int(final_time / time_step_size)
 
-        Simulation.setup_default_lattice_phenotypes()
+        self.setup_default_lattice_phenotypes(absolute_max_phenotype, no_possible_phenotypes)
 
         """
         
@@ -539,7 +221,6 @@ class Simulation:
         sequence_matrix = np.identity(10)
         affinity_matrix = np.identity(10)
 
-    @classmethod
     def setup_default_lattice_phenotypes(self, absolute_max_phenotype, no_possible_phenotypes):
         """
         If you are using a lattice phenotype, setup and get the PhenotypeStructure and PhenotypeInteractions
@@ -551,7 +232,6 @@ class Simulation:
             self.phen_struct
         )
 
-    @classmethod
     def setup_default_sequence_phenotypes(self, possible_tumours : list[str], possible_CTLs : list[str], sequence_matrix, affinity_matrix):
         """
         
--- sim/readme.md
+++ sim/readme.md
@@ -1,6 +1,20 @@
 ## Configurations
 This section describes what parameters need to be specified to run the various simulations.
 
+The configuration loaded is dependent on:
+- `simtype` (this will come from the file you run: `continuous_run.py` or `run.py` i.e. whether you run Marta's model, or the discrete model)
+`discrete`
+`continuous`
+- `subtype` (specified in the config file, and defaulting to `lattice`)
+
+`name` must be unique.
+
+
+All arguments ar either:
+- required (0)
+- optional (1)
+- ignored (2)
+
 ### Marta's Continuous Model
 
 Required:
@@ -29,7 +43,7 @@ Optional:
 name
 
 Not required (could be made optional):
-binding_affinity_matrix_config
+affinity_matrix_config
 print_results?
 
 
@@ -61,3 +75,36 @@ CTL_natural_prolif_rate
 CTL_natural_death_rate
 CTL_interaction_induced_rate
 CTL_selectivity
+
+> subtype
+
+### Sequence
+
+Required:
+
+name
+
+time_step
+final_time
+
+no_possible_phenotypes
+no_init_tumour_cells
+no_init_CTL_cells
+
+affinity_range
+// binding_affinity
+tumour_phenotypic_variation_probability
+
+tumour_natural_prolif_rate
+tumour_natural_death_rate
+tumour_interaction_induced_rate
+tumour_selectivity
+
+CTL_natural_prolif_rate
+CTL_natural_death_rate
+CTL_interaction_induced_rate
+CTL_selectivity
+
+> subtype
+> sequence_matrix_config
+> affinity_matrix_config
