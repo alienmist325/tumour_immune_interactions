6c959bc3c764abddf70944ea4ee0bd2d353271d0
Akesh Mistry <akeshmistry15@gmail.com>
2024-06-04 01:31:05 +0100

    Fix Fish Plotting

--- sim/graphing.py
+++ sim/graphing.py
@@ -110,7 +110,7 @@ def fish(sim: Simulation, bundle_name, absolute=False):
             np.array(parent_tree), columns=["ParentId", "ChildId"]
         )
         data = process_data(populations_df, parent_tree_df, absolute=absolute)
-        setup_figure(width=1000, height=500)
+        setup_figure(width=1200, height=1000)
         times = np.linspace(0, sim.time_step * sim.time_step_size, sim.time_step)
         data = list(data)
         data[1] = times
@@ -118,6 +118,34 @@ def fish(sim: Simulation, bundle_name, absolute=False):
 
         fish_plot(*data)
         return plt
+
+
+def report_fish(sim: Simulation, bundle_name, absolute=False):
+    import matplotlib.pyplot as plt
+
+    plt.rcParams["font.size"] = 25
+    plt.rcParams["lines.linewidth"] = 4
+    plt.rcParams["xtick.labelbottom"] = True
+
+    plot = fish(sim, bundle_name, absolute)
+    plot.ylim([0.5, 1])
+    plot.yticks([])
+    plot.ylabel("")
+    plot.xlabel("Days")
+
+    state = next(sim.history.__iter__())
+    bundle = get_bundle(state, bundle_name)
+    sequences = bundle.phen_struct.sequences
+
+    dist = max([len(seq) for seq in sequences])
+
+    top = 0.965
+    left = -dist
+    dtop = 0.5 / len(sequences)
+
+    for seq in sequences:
+        plot.text(left, top, seq)
+        top -= dtop
 
 
 def get_bundle(state: SimulationState, bundle_name) -> CellBundle:
@@ -197,11 +225,14 @@ plt_fn_label = {
     hist: "hist",
     fish_tumour: "fish_tumour",
     fish_CTL: "fish_CTL",
+    report_graph: "graph_report",
 }
 
 
 def savefig(
-    sim: Simulation = None, plt_fn: Callable = graph, path_to_output=path_to_output
+    sim: Simulation = None,
+    plt_fn: Callable = report_graph,
+    path_to_output=path_to_output,
 ):
     if sim is None:
         sim = get_sim()
--- sim/test.py
+++ sim/test.py
@@ -3,4 +3,10 @@ with open("debug.txt", "a") as file:
     deaths = 1
     line = str(births) + "," + str(deaths) + "\n"
     file.write(line)
-    file.write("hello", 'a')
+    file.write("hello", "a")
+
+
+from pyfish import setup_figure, process_data, fish_plot
+
+setup_figure()
+process_data()
